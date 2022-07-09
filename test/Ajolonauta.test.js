3a3dd97cb3909e9e1126aa898e5971f79d885912
Mike&rade <hoodloom07@outlook.es>
2022-07-08 23:56:33 -0500

    ajustes de test jest & primeras pruebas

---
+++ test/Ajolonauta.test.js
@@ -0,0 +1,11 @@
+const Ajolonauta = require("./../app/ajolonauta");
+
+describe("Caso de prueba 1 de clase Ajolonauta", () => {
+    test('creacion de clase Ajolonauta', () => {
+        const ajo = new Ajolonauta("Ajo");
+        const ajo2 = new Ajolonauta("Woopa");
+
+        expect(ajo.name).toBe("Ajo");
+        expect(ajo2.name).toBe("Woopa");
+    })
+})
