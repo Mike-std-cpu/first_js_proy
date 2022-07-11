2e81e32f464c51752d21d23f184039778fbd5fdb
Mike&rade <hoodloom07@outlook.es>
2022-07-11 11:44:28 -0500

    Implementacion de pruebas a index

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const Ajolonauta = require("././app/ajolonauta");
+const AjoHero = require("././app/ajoHero");
 
 const woopa = new Ajolonauta("Woopa");
 console.log(`¡Hola! mi nombre es ${woopa.name}`);
+
+//Test 2 aprobado
+const ironman = new AjoHero("Tony Stark","Iron Ajo","Woppa");
+ console.log(`Soy ${ironman.realName} e interpreto a ${ironman.name} como ${ironman.hero}`);
